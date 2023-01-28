94ad365f3e7e17a8662d92189e7c0ac8a0a3d481
minseo-hong <mshong1007@gmail.com>
2023-01-28 13:23:23 +0900

    Add header.h

--- Graph/main.c
+++ Graph/main.c
@@ -1,24 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define TRUE    1
-#define FALSE   0
-#define VERTEX_MAX_SIZE    10
-
-typedef struct Graph {
-    int size;
-    int adjMatrix[VERTEX_MAX_SIZE][VERTEX_MAX_SIZE];
-} Graph;
-
-Graph* create(void);
-int isEmpty(Graph *graph);
-int isFull(Graph *graph);
-void insertVertex(Graph *graph, int vertex);
-int isEdgeNumError(Graph *graph, int start, int end);
-void insertEdge(Graph *graph, int start, int end);
-void printAdjMatrix(Graph *graph);
-void deleteEdge(Graph *graph, int start, int end);
-void deleteVertex(Graph *graph, int vertex);
+#include "header.h"
 
 int main(void) {
     Graph *graph = create();
